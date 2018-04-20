0bbf668e96d0df761b3670baffe7c224daa00a19
Leander Jedamus <ljedamus@web.de>
2018-04-20 08:37:11 +0200

    added backup system

---
+++ backup_neu.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# erzeugt Mittwoch, 07. Juni 2017 19:34 (C) 2017 von Leander Jedamus
+# modifiziert Freitag, 20. April 2018 08:29 von Leander Jedamus
+# modifiziert Montag, 16. April 2018 21:14 von Leander Jedamus
+# modifiziert Mittwoch, 11. Oktober 2017 18:43 von Leander Jedamus
+# modifiziert Samstag, 10. Juni 2017 09:44 von Leander Jedamus
+# modifiziert Freitag, 09. Juni 2017 15:09 von Leander Jedamus
+# modifiziert Donnerstag, 08. Juni 2017 16:14 von Leander Jedamus
+# modifiziert Mittwoch, 07. Juni 2017 19:52 von Leander Jedamus
+
+#set -x
+
+. $HOME/bin/backup_include_neu.sh
+
+TARFILE="$FILENAME.tar.$SUFFIX"
+
+cd /
+sudo tar cvf$TARSUFFIX "$TARFILE" --exclude-from=$HOME/bin/exclude-from.txt ./etc ./root ./home ./Bilder ./data ./bin ./sbin ./var ./boot ./vmlinuz ./vmlinuz.old ./initrd.img ./initrd.img.old ./usr ./lib ./lib32 ./lib64 ./libx32 ./opt ./srv > "$LOGFILE" 2> "$ERRFILE"
+
+# vim:ai sw=2
+
---
+++ tailbackup.txt_neu.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# erzeugt Mittwoch, 07. Juni 2017 19:34 (C) 2017 von Leander Jedamus
+# modifiziert Freitag, 20. April 2018 08:30 von Leander Jedamus
+# modifiziert Montag, 16. April 2018 13:14 von Leander Jedamus
+# modifiziert Mittwoch, 13. September 2017 15:27 von Leander Jedamus
+# modifiziert Samstag, 10. Juni 2017 09:44 von Leander Jedamus
+# modifiziert Freitag, 09. Juni 2017 15:09 von Leander Jedamus
+# modifiziert Donnerstag, 08. Juni 2017 16:14 von Leander Jedamus
+# modifiziert Mittwoch, 07. Juni 2017 19:52 von Leander Jedamus
+
+#set -x
+
+. $HOME/bin/backup_include_neu.sh
+
+tail -f "$LOGFILE"
+
+# vim:ai sw=2
+
