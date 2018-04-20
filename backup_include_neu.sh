36958682a2e1e6f37338e0d304d77b22235d7b38
Leander Jedamus <ljedamus@web.de>
2018-04-20 11:11:21 +0200

    added ~/.backupdir

--- backup_include_neu.sh
+++ backup_include_neu.sh
@@ -1,12 +1,11 @@
 
 
 # erzeugt Mittwoch, 07. Juni 2017 19:34 (C) 2017 von Leander Jedamus
-# modifiziert Freitag, 20. April 2018 08:35 von Leander Jedamus
+# modifiziert Freitag, 20. April 2018 11:09 von Leander Jedamus
 # modifiziert Montag, 16. April 2018 14:55 von Leander Jedamus
 
 DATE=`date +"%d.%m.%Y"`
-FILEPATH="/media/leander/Expansion Drive/Backup"
-#FILEPATH="/mnt"
+FILEPATH="$HOME/.backupdir/Backup"
 FILENAME="$FILEPATH/ubuntu_16.04LTS_neu_$DATE"
 
 SUFFIX=gz
