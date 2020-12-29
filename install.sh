ea44b05cfc4bb7461220c5488d5f43c2b56547d5
Leander Jedamus <ljedamus@web.de>
2020-12-29 18:33:55 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# erzeugt Dienstag, 29. Dezember 2020 18:28 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 29. Dezember 2020 18:32 von Leander Jedamus
+
+cp -vp backup*.sh tailbackup*.sh $HOME/bin
+cp -Rvp .tmux.conf .tmuxinator $HOME
+
+# vim:ai sw=2
+
