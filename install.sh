8e978dabefa71cb85e09162bad9115e88c3ece39
Leander Jedamus <ljedamus@web.de>
2022-03-07 20:13:14 +0100

    changed to install ssh.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # erzeugt Dienstag, 29. Dezember 2020 18:28 (C) 2020 von Leander Jedamus
 # modifiziert Dienstag, 29. Dezember 2020 18:32 von Leander Jedamus
 
-cp -vp backup*.sh tailbackup*.sh $HOME/bin
+cp -vp backup*.sh tailbackup*.sh ssh.sh $HOME/bin
 cp -Rvp .tmux.conf .tmuxinator $HOME
 
 # vim:ai sw=2
