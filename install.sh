9420f090d1aa8cb6cbe4617d3c5d37d9db55ea2f
Leander Jedamus <ljedamus@web.de>
2022-03-15 20:09:07 +0100

    added mkdir line to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # erzeugt Dienstag, 29. Dezember 2020 18:28 (C) 2020 von Leander Jedamus
 # modifiziert Dienstag, 29. Dezember 2020 18:32 von Leander Jedamus
 
+mkdir -p $HOME/bin
 cp -vp backup*.sh tailbackup*.sh ssh.sh $HOME/bin
 cp -Rvp .tmux.conf .tmuxinator $HOME
 
