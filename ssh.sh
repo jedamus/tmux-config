ae7b3bfff2202c7ce9967cc27b5d27f274214795
Leander Jedamus <ljedamus@web.de>
2022-03-19 10:13:13 +0100

    changed to use ~/.ssh/private_key_list

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+pfad=$HOME/.ssh
+private_key_list=$pfad/private_key_list
+
 eval "$(ssh-agent -s)"
-ssh-add $HOME/.ssh/id_rsa
+
+if [ -f $private_key_list ]; then
+  private_keys=$(cat $private_key_list)
+else
+  private_keys="id_rsa"
+fi
+
+for private_key in $private_keys; do
+  ssh-add $pfad/$private_key
+done
+
 tmuxinator ssh
