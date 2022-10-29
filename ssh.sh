04fca776faeb18c59b4e5ccd8571132838be5388
Leander Jedamus <jedamus@users.noreply.github.com>
2022-10-29 15:01:07 +0200

    added parameters to ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -5,10 +5,14 @@ private_key_list=$pfad/private_key_list
 
 eval "$(ssh-agent -s)"
 
-if [ -f $private_key_list ]; then
-  private_keys=$(cat $private_key_list)
+if [ -z $1 ]; then
+  if [ -f $private_key_list ]; then
+    private_keys=$(cat $private_key_list)
+  else
+    private_keys="id_rsa"
+  fi 
 else
-  private_keys="id_rsa"
+  private_keys=$*  
 fi
 
 for private_key in $private_keys; do
